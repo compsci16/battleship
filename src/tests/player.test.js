9986d8a747f023797abda62063e75588b2b9179f
wint <wintcred@gmail.com>
2021-07-10 02:03:03 +0530

    Refactored test to include player names

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -7,7 +7,9 @@ describe("player", () => {
   it("throws error without board access", () => {
     const ownBoard = new Gameboard(2);
     expect(() => new Player()).toThrowError("No board for player");
-    expect(() => new Player(ownBoard)).toThrowError("No opponent board access");
+    expect(() => new Player("p1", ownBoard)).toThrowError(
+      "No opponent board access"
+    );
   });
 
   it("receives passed board", () => {
@@ -24,8 +26,8 @@ describe("player", () => {
     const oppBoard = new Gameboard(2);
     const ship = new Ship(2);
     oppBoard.placeShip(ship, 0, 0);
-    const player1 = new Player(ownBoard, oppBoard);
-    const player2 = new Player(oppBoard, ownBoard);
+    const player1 = new Player("p1", ownBoard, oppBoard);
+    const player2 = new Player("p2", oppBoard, ownBoard);
     player1.attack(0, 0);
     expect(oppBoard.grid[0][0].attacked).toBe(true);
   });
@@ -34,8 +36,8 @@ describe("player", () => {
     const ownBoard = new Gameboard(2);
     const oppBoard = new Gameboard(2);
     oppBoard.placeShip(new Ship(2), 0, 0);
-    const player1 = new Player(ownBoard, oppBoard);
-    const player2 = new Player(oppBoard, ownBoard);
+    const player1 = new Player("p1", ownBoard, oppBoard);
+    const player2 = new Player("p2", oppBoard, ownBoard);
     player1.attack(0, 0);
     player1.attack(0, 1);
     expect(oppBoard.grid[0][0].ship.isSunk()).toBe(true);
