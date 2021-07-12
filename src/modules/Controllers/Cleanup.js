01ded34bb910faf7d3f9a524e25af0710007f978
wint <wintcred@gmail.com>
2021-07-12 22:06:34 +0530

    Added status box, icon pointer

--- src/modules/Controllers/Cleanup.js
+++
@@ -1,7 +0,0 @@
-import { blockClickHandler } from "./BlockClick";
-export default function cleanup() {
-  document.querySelectorAll(".grid .block").forEach((block) => {
-    block.removeEventListener("click", blockClickHandler);
-  });
-  document.removeEventListener("click", document.fn);
-}
